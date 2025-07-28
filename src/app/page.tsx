378be008d185f28c6d8f730b828255dc86cd6a62
Susheel Kumar <thotasusheelkumar04@gmail.com>
2025-07-28 08:00:10 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Header from '@/components/layout/header';
 import Hero from '@/components/sections/hero';
-import TrustedBy from '@/components/sections/trusted-by';
 import Services from '@/components/sections/services';
 import Process from '@/components/sections/process';
 import Testimonials from '@/components/sections/testimonials';
@@ -14,7 +13,6 @@ export default function Home() {
       <Header />
       <main className="flex-grow">
         <Hero />
-        <TrustedBy />
         <Services />
         <Process />
         <Testimonials />
